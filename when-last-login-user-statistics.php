bbfeae6096157890f0fc02cebf3c2d0e21c3701c
Jarryd Long <JarrydLong@users.noreply.github.com>
2022-11-30 19:56:56 +0200

    Removed code that applies to V2

--- when-last-login-user-statistics.php
+++ when-last-login-user-statistics.php
@@ -370,155 +370,90 @@ class WhenLastLoginStatistics{
 
 		$wll_version = floatval( get_option( 'wll_current_version' ) );
 
-		if( version_compare( $wll_version, 1.2, '>=' ) ){
+		if( $additional_args['range'] == 'today' ){
 
-			global $wpdb;
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'wll_records',
+				'date_query' => array(
+					'after' => 'today',
+					'inclusive' => true
+				)
+			);
 
-        	$records = $wpdb->prefix.'wll_login_records';
+		} else if( $additional_args['range'] == '-7days' ){
 
-        	$sql = "SELECT * FROM $records ";
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'wll_records',
+				'date_query' => array(
+					'before' => '7 Days'
+				)
+			);
 
-			if( $additional_args['range'] == 'today' ){
+		} else if( $additional_args['range'] == '-30days' ){
 
-				$sql .= "WHERE `date` > '".date( 'Y-m-d', current_time('timestamp') )."' ORDER BY `id` DESC";
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'wll_records',
+				'date_query' => array(
+					'before' => '30 Days'
+				)
+			);
 
-			} else if( $additional_args['range'] == '-7days' ){
+		} else if( $additional_args['range'] == 'custom' ){
 
-				$today = date( 'Y-m-d', current_time('timestamp') );
-				$previous = date( 'Y-m-d', strtotime( $today.' - 7 DAY' ) );
+			$new_start_date = strtotime( $additional_args['start_date'] .' - 1 day' );
+			$new_end_date =  strtotime( $additional_args['end_date'] .' + 1 day' );
 
-				$sql .= "WHERE `date` > '".$previous."' ORDER BY `id` DESC";
-
-			} else if( $additional_args['range'] == '-30days' ){
-
-				$today = date( 'Y-m-d', current_time('timestamp') );
-				$previous = date( 'Y-m-d', strtotime( $today.' - 30 DAY' ) );
-				
-				$sql .= "WHERE `date` > '".$previous."' ORDER BY `id` DESC";
-
-			} else if( $additional_args['range'] == 'custom' ){
-
-				$new_start_date = strtotime( $additional_args['start_date'] .' - 1 day' );
-				$new_end_date =  strtotime( $additional_args['end_date'] .' + 1 day' );
-
-				$sql .= "WHERE `date` > '".$new_start_date."' AND `date` < '".$new_end_date."' ORDER BY `id` DESC";
-
-			} else {
-
-				$sql .= "ORDER BY `id` DESC";
-
-			}
-
-			$args = apply_filters( 'wll_return_stats_wpquery_filter', $sql );
-
-			$login_record_array = array();
-
-			$results = $wpdb->get_results( $sql );
-
-			if( !empty( $results ) ){
-
-				foreach( $results as $result ){
-
-					$activity_last_login = get_user_meta( intval( $result->author ), 'when_last_login', true );
-
-					$login_record_array[] = array(
-						'author_name' => $result->title,
-						'author_id' => $result->author,
-						'date' => strtotime( $result->date ),
-						'last_login_timestamp' => (int) $activity_last_login
-					);
-
-				}
-			}
-			
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'wll_records',
+				'date_query' => array(
+					'after' => date('Y-m-d', $new_start_date ),
+					'before' => date('Y-m-d', $new_end_date ),
+					'inclusive' => true
+				)
+			);
 
 		} else {
 
-			if( $additional_args['range'] == 'today' ){
+			$args = array(
+				'posts_per_page' => -1,
+				'post_type' => 'wll_records'
+			);
 
-				$args = array(
-					'posts_per_page' => -1,
-					'post_type' => 'wll_records',
-					'date_query' => array(
-						'after' => 'today',
-						'inclusive' => true
-					)
-				);
+		}
 
-			} else if( $additional_args['range'] == '-7days' ){
+		$args = apply_filters( 'wll_return_stats_wpquery_filter', $args );			
 
-				$args = array(
-					'posts_per_page' => -1,
-					'post_type' => 'wll_records',
-					'date_query' => array(
-						'before' => '7 Days'
-					)
-				);
+		$the_query = new WP_Query( $args );
 
-			} else if( $additional_args['range'] == '-30days' ){
+		if ( $the_query->have_posts() ) {
 
-				$args = array(
-					'posts_per_page' => -1,
-					'post_type' => 'wll_records',
-					'date_query' => array(
-						'before' => '30 Days'
-					)
-				);
+			while ( $the_query->have_posts() ) {
 
-			} else if( $additional_args['range'] == 'custom' ){
+				$the_query->the_post();
 
-				$new_start_date = strtotime( $additional_args['start_date'] .' - 1 day' );
-				$new_end_date =  strtotime( $additional_args['end_date'] .' + 1 day' );
+				$activity_author = get_the_author();
+				$activity_date = get_the_date( 'Y-m-d H:i:s' );
 
-				$args = array(
-					'posts_per_page' => -1,
-					'post_type' => 'wll_records',
-					'date_query' => array(
-						'after' => date('Y-m-d', $new_start_date ),
-						'before' => date('Y-m-d', $new_end_date ),
-						'inclusive' => true
-					)
-				);
+				$activity_last_login = get_the_author_meta( 'when_last_login' );
+				$activity_author_id = get_the_author_meta( 'ID' );
 
-			} else {
-
-				$args = array(
-					'posts_per_page' => -1,
-					'post_type' => 'wll_records'
+				$login_record_array[] = array(
+					'author_name' => $activity_author,
+					'author_id' => $activity_author_id,
+					'date' => strtotime( $activity_date ),
+					'last_login_timestamp' => (int) $activity_last_login
 				);
 
 			}
 
-			$args = apply_filters( 'wll_return_stats_wpquery_filter', $args );			
+			wp_reset_postdata();
 
-			$the_query = new WP_Query( $args );
+		}
 
-			if ( $the_query->have_posts() ) {
-
-				while ( $the_query->have_posts() ) {
-
-					$the_query->the_post();
-
-					$activity_author = get_the_author();
-					$activity_date = get_the_date( 'Y-m-d H:i:s' );
-
-					$activity_last_login = get_the_author_meta( 'when_last_login' );
-					$activity_author_id = get_the_author_meta( 'ID' );
-
-					$login_record_array[] = array(
-						'author_name' => $activity_author,
-						'author_id' => $activity_author_id,
-						'date' => strtotime( $activity_date ),
-						'last_login_timestamp' => (int) $activity_last_login
-					);
-
-				}
-
-				wp_reset_postdata();
-
-			}
-
-		}		
 
 		return $login_record_array;
 
